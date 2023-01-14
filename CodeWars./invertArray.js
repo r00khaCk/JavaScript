8453871a463a7e49948338fd02f743a5f4de4ad1
Deilan Raj Victor Jonathan <76257057+r00khaCk@users.noreply.github.com>
2023-01-14 13:08:37 +0800

    Upload invertArray.js
    Code used to invert the values in an array.

---
+++ CodeWars./invertArray.js
@@ -0,0 +1,20 @@
+````js
+var myArray = [1,2,3,4,5];
+
+function invert(array){
+    var invertArray = [];
+    for (var i=0; i < array.length; i++){ // this loop will go through each value in the array based on the length of the array given
+        
+        if (Math.sign(array[i]) == 1){ // Math.sign() is used to determine whether the value is negative (== -1) or positive (== 1).
+            invertArray.push(-Math.abs(array[i])); //Math.abs is used to convert a value to the absolute value. -Math.abs() converts value to negative and Math.abs converts value to positive.
+        }
+        else if (Math.sign(array[i]) == -1){
+            invertArray.push(Math.abs(array[i]));
+        }
+        
+    }
+    return invertArray;
+}
+console.log(invert([]));
+
+````
