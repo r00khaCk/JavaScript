0935e0ef924f21b55448420807f267a28e68313e
Deilan Raj Victor Jonathan <76257057+r00khaCk@users.noreply.github.com>
2023-01-14 13:10:46 +0800

    Added the question

--- CodeWars./invertArray.js
+++ CodeWars./invertArray.js
@@ -1,3 +1,7 @@
+/**Instruction:
+Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+**/
+
 ````js
 var myArray = [1,2,3,4,5];
 
